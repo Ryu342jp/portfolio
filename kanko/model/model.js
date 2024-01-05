6680be67753dea60695070b68b5de530c4457750
Ryu342jp <134501131+Ryu342jp@users.noreply.github.com>
2024-01-05 10:21:31 +0900

    Delete kanko directory

--- kanko/model/model.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navbarMenu = document.querySelector(".navbar-menu");
-
-    menuToggle.addEventListener("click", function() {
-        navbarMenu.classList.toggle("active");
-    });
-});
